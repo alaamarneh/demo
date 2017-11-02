e152b95533a8717c9e94e25b27dfbd37b9674207
alaamarneh <ala.amarneh@hotmail.com>
2017-11-02 13:23:22 +0200

    Demo respsitory
    Demo Android library

---
+++ src/main/java/com/ala/demo1/BeanDemo.java
@@ -0,0 +1,17 @@
+package com.ala.demo1;
+
+/**
+ * Created by alaam on 11/2/2017.
+ */
+
+public class BeanDemo {
+    private String param;
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
+}
